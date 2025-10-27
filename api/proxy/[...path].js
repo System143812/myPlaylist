3ef4b1873aaf10f64cc24dd49ae3186d73a59d89
System143812 <mypubacc0unt@gmail.com>
2025-10-27 18:00:49 +0800

    debug malala9

--- api/proxy/[...path].js
+++ api/proxy/[...path].js
@@ -9,6 +9,7 @@ const BACKEND_URL = process.env.BACKEND_URL;
 export default async function handler(req, res) {
   try {
     const { path } = req.query;
+    return res.status(200).json({message: path});
     const urlPath = Array.isArray(path) ? path.join('/') : path;
     const backendUrl = `${BACKEND_URL}/${urlPath}`;
 
