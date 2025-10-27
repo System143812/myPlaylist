3f4022fefb99d1ce854392c8c06501a75e8ec51d
System143812 <mypubacc0unt@gmail.com>
2025-10-27 18:04:24 +0800

    debug malala10 umay sayo vercel

--- api/proxy/[...path].js
+++ api/proxy/[...path].js
@@ -8,7 +8,7 @@ const BACKEND_URL = process.env.BACKEND_URL;
 
 export default async function handler(req, res) {
   try {
-    const { path } = req.query;
+    const path = req.url.replace("/api/proxy", "");
     return res.status(200).json({message: path});
     const urlPath = Array.isArray(path) ? path.join('/') : path;
     const backendUrl = `${BACKEND_URL}/${urlPath}`;
