2d422c1d6955aba33f77bf14d53ff5c65d9a492f
System143812 <mypubacc0unt@gmail.com>
2025-10-27 19:56:59 +0800

    debug malala umay sayo vercel 10

--- api/proxy/[...path].js
+++ api/proxy/[...path].js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
   try {
     
     const urlPath = req.query["...path"];
-    const backendUrl = `${BACKEND_URL}${urlPath}`;
+    const backendUrl = `${BACKEND_URL}/${urlPath}`;
 
     const response = await fetch(backendUrl, {
       method: req.method,
